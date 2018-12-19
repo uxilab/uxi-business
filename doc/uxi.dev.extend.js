febc68a881fdf0200c46b76f073f7fd2aced2e36
Denis Florkin <denisflorkin@gmail.com>
2018-12-19 17:52:15 +0100

    updated doc project webpack config extender

--- doc/uxi.dev.extend.js
+++ doc/uxi.dev.extend.js
@@ -1,17 +1,17 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = (config) => {
-  config.devServer.port = 3300;
+  config.devServer.port = 3300; // eslint-disable-line no-param-reassign
   config.entry[1].replace(/(:\d*)$/, ':3300');
 
 
-  config.resolve.alias = {
+  config.resolve.alias = { // eslint-disable-line no-param-reassign
     'uxi-business': path.resolve(__dirname, '../src'),
     'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
     'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    'react': path.resolve(__dirname, 'node_modules/react'),
+    react: path.resolve(__dirname, 'node_modules/react'),
     'redux-form': path.resolve(__dirname, 'node_modules/redux-form'),
-    'uxi': path.resolve(__dirname, 'node_modules/uxi'),
+    uxi: path.resolve(__dirname, 'node_modules/uxi'),
     'react-intl': path.resolve(__dirname, 'node_modules/react-intl'),
   };
 
