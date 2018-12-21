4104239115623797d800b6e2ff97806bc0768212
Denis Florkin <denisflorkin@gmail.com>
2018-12-21 17:38:29 +0100

    expose DefaultUserFeedback under userMessage update peerDeps

--- src/userMessage/index.js
+++ src/userMessage/index.js
@@ -17,6 +17,7 @@ export reducer from './reducer';
 export withDefaultErrorHandlingActions from './actions';
 export withUserMessage from './components/hocs/withUserMessage';
 export UserMessageProvider from './components/UserMessageProvider';
+export DefaultUserFeedback from './components/DefaultUserFeedback';
 
 const withUserMessageAction = (Comp, contextId) => {
   const mapDispatchToProps = dispatch => ({
