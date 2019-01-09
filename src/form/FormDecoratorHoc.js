d54d4ff79b6e8c3eca570b4be902e514c5d5e160
Denis Florkin <denisflorkin@gmail.com>
2019-01-09 17:03:16 +0100

    Revert "lint: fix (ignore) label issue"
    This reverts commit fa97dfb7af7000f650359b250dfb258502c5a54a.

--- src/form/FormDecoratorHoc.js
+++ src/form/FormDecoratorHoc.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Text from 'uxi/Text';
-// import RequiredField from './RequiredField';
+import RequiredField from './RequiredField';
 import InputFieldGroup from './InputFieldGroup';
 
 const FormDecorator = (Input, props) => field => (
   <div>
-    {/* <div>
-        {
-          !props.noLabel && props.label &&
+    <div>
+      {
+        !props.noLabel && props.label &&
         (
           <label>
             {props.label}
             {props && props.isRequired && <RequiredField />}
           </label>
         )
-        }
-      </div>
-      */}
+      }
+    </div>
     {props.helpText &&
       <div style={{ marginBottom: '12px' }}>
         <Text type="caption">
@@ -25,7 +24,7 @@ const FormDecorator = (Input, props) => field => (
       </div>
     }
     {
-      field.meta.asyncValidating && 'Loading!!!'
+      field.meta.asyncValidating && 'Loadingg!!!'
     }
     <div style={{ marginBottom: '16px' }}>
       {
